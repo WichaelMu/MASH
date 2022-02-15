901d6114228980f6cf233b6ae3306ca556fef4ff
WichaelMu <69779024+WichaelMu@users.noreply.github.com>
2022-02-15 12:36:49 +1100

    Audio

--- Assets/Scripts/HelicopterPickup.cs
+++ Assets/Scripts/HelicopterPickup.cs
@@ -1,18 +1,61 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class HelicopterPickup : MonoBehaviour
 {
+	public static Action<bool> OnGameStatus;
 
+	[SerializeField] TMPro.TextMeshProUGUI Counter;
+
+	const string kRescured = "Soldiers Rescured: ";
+	const string kInHeli = "Soldiers in Helicopter: ";
+
+	uint SoldiersInHospital = 0;
 	uint SoldiersInHelicopter = 0;
+
+	int AllSoldiers = 0;
+
+	void Start()
+	{
+		UpdateCounter();
+
+		AllSoldiers = GameObject.FindGameObjectsWithTag("Soldier").Length;
+	}
 
 	void OnTriggerEnter2D(Collider2D collision)
 	{
 		if (SoldiersInHelicopter < 3 && collision.CompareTag("Soldier"))
 		{
 			SoldiersInHelicopter++;
-			Debug.Log("pickup");
+			Audio.AAudioInstance.Play("ONPICKUP");
 		}
+
+		if (collision.CompareTag("Hospital"))
+		{
+			SoldiersInHospital += SoldiersInHelicopter;
+			SoldiersInHelicopter = 0;
+
+			if (SoldiersInHospital == AllSoldiers)
+			{
+				DisplayGameStatus(false);
+			}
+		}
+
+		if (collision.CompareTag("Tree"))
+		{
+			DisplayGameStatus(true);
+		}
+
+		UpdateCounter();
+	}
+
+	void UpdateCounter()
+	{
+		Counter.text = kRescured + SoldiersInHospital + "\n" + kInHeli + SoldiersInHelicopter;
+	}
+
+	void DisplayGameStatus(bool bIsGameOver)
+	{
+		OnGameStatus?.Invoke(bIsGameOver);
 	}
 }
